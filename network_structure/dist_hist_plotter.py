ffecafafbc8c198a546656812de6ac998f2bd8a1
Wouter Kayser <wouterkayser@gmail.com>
2020-10-25 12:33:25 +0100

    Updates to inter site distance plots and the locations of cells.

--- network_structure/dist_hist_plotter.py
+++ network_structure/dist_hist_plotter.py
@@ -12,13 +12,13 @@ dists = json.load(open('dists.json', 'r'))
 # Remove all values that could not find a close  cell.
 [dists.pop(key) for key in [key for key in dists if dists[key] == 20000]]
 
-# calculate average and print it. 
+# calculate average and print it.
 print(f'The average distance is {sum(dists.values()) / len(dists.values())}')
 
-plt.subplots(figsize=(8*0.8, 5))
+plt.subplots(figsize=(4.5*1.4, 4.5))
 # Truncate to 10 km, as only few cells are outside this window
 plt.hist(dists.values(), bins=100, range=(0, 10000))
 plt.xlim(0, 10000)
-plt.xlabel('Smallest Inter Site Distance (m)')
-plt.ylabel('Occurrences per cell')
+plt.xlabel('Inter Site Distance (m)')
+plt.ylabel('Occurrence')
 plt.savefig('disthist.pdf')
